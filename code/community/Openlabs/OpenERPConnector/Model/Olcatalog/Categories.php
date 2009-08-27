dee7109374b002c39d02ecd6d63d3e430cb1e5e6
Sharoon Thomas <sharoonthomas@teagarden.in>
2009-08-27 12:40:55 +0530

    [imp]

--- code/community/Openlabs/OpenERPConnector/Model/Olcatalog/Categories.php
+++ code/community/Openlabs/OpenERPConnector/Model/Olcatalog/Categories.php
@@ -24,18 +24,14 @@ class Openlabs_OpenERPConnector_Model_Olcatalog_Categories extends Mage_Catalog_
 				// If we are adding filter on non-existent attribute
 			}
 		}
-
 		$result = array ();
 		foreach ($collection as $category) {
 			//$result[] = $customer->toArray();
 			$result[] = array (
 				'category_id' => $category->getId(),
 				'image' => $category->getImage(),
-
-				
 			);
 		}
-
 		return $result;
 	}
 
@@ -55,6 +51,7 @@ class Openlabs_OpenERPConnector_Model_Olcatalog_Categories extends Mage_Catalog_
 								$fp = fopen($fullpath, "rb");
 								$imagebin = fread($fp, filesize($fullpath));
 								$img_data = base64_encode($imagebin);
+								fclose($fp);
 							} catch (Exception $e) {
 								return "exc proc";
 							}
@@ -77,51 +74,23 @@ class Openlabs_OpenERPConnector_Model_Olcatalog_Categories extends Mage_Catalog_
 
 	}
 
-	public function create($groupdata) {
-		try {
-			$group = Mage :: getModel('core/store_group')->setData($groupdata)->save();
-
-		} catch (Magento_Core_Exception $e) {
-			$this->_fault('data_invalid', $e->getMessage());
-		} catch (Exception $e) {
-			$this->_fault('data_invalid', $e->getMessage());
-		}
-		return $group->getId();
-	}
-
-	public function update($groupid, $groupdata) {
-		try {
-			$group = Mage :: getModel('core/store_group')->load($groupid);
-			if (!$group->getId()) {
-				$this->_fault('group_not_exists');
+	public function create($filename, $imagedata) {
+		if ($filename and $imagedata) {
+			try {
+				if ($filename) {
+					$path = Mage :: getBaseDir('media') . DS . 'catalog' . DS . 'category' . DS;
+					$fullpath = $path . $filename;
+					try {
+						$fp = fopen($fullpath, "wb");
+						$image = fwrite($fp, base64_decode($imagedata));
+						fclose($fp);
+					} catch (Exception $e) {
+						return "exc proc";
+					}
+				}
+				return true;
 			}
-			$group->addData($groupdata)->save();
-		} catch (Magento_Core_Exception $e) {
-			$this->_fault('data_invalid', $e->getMessage());
-		} catch (Exception $e) {
-			$this->_fault('data_invalid', $e->getMessage());
 		}
-		return true;
-	}
-
-	public function delete($groupid) {
-		try {
-			$group = Mage :: getModel('core/store_group')->load($groupid);
-			if (!$group->getId()) {
-				$this->_fault('group_not_exists');
-			}
-			$group->delete();
-
-		} catch (Magento_Core_Exception $e) {
-			$this->_fault('data_invalid', $e->getMessage());
-		} catch (Exception $e) {
-			$this->_fault('data_invalid', $e->getMessage());
-		}
-		return true;
-	}
-
-	protected function _getResource() {
-		return Mage :: getResourceSingleton('catalog/category_attribute_backend_media');
 	}
 }
 ?>
